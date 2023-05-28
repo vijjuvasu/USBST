1f8cea8d4c9631b909f19b69f17945fe67289dce
vijjuvasu <134858224+vijjuvasu@users.noreply.github.com>
2023-05-28 18:07:10 -0400

    b

--- src/testng/Googlelaunch.java
+++ src/testng/Googlelaunch.java
@@ -1,14 +1,20 @@
 package testng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Googlelaunch {
+	WebDriver driver;
   @Test
   public void f() {
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.quit();
   }
 
 }
